6dacc2e9e83aa6a97a8e1bc4e3e843babf146644
karandashov <vlad.karandashov.tech@gmail.com>
2024-01-07 12:37:08 +0300

    start

---
+++ src/main/java/ru/abradox/yandexgateway/YandexGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.abradox.yandexgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/abradox/yandexgateway/YandexGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abradox.yandexgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
