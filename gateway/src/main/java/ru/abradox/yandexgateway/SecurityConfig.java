010268e88429df1c88d797d93fcc4d0d7bfbd083
karandashov <vlad.karandashov.tech@gmail.com>
2024-01-07 18:03:57 +0300

    понизил версию и оставил в коментариях тестовые попытки

---
+++ gateway/src/main/java/ru/abradox/yandexgateway/SecurityConfig.java
@@ -0,0 +1,48 @@
+package ru.abradox.yandexgateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAttributeHandler;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+@Configuration
+class SecurityConfig {
+
+//    @Bean
+//    public SecurityWebFilterChain clientSecurityFilterChain(ServerHttpSecurity http) {
+//        return http
+//                //.csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                //.cors(ServerHttpSecurity.CorsSpec::disable)
+//                .authorizeExchange(authorize -> authorize
+//                        .anyExchange().permitAll().and().oauth2Login())
+//                .build();
+//    }
+
+//    @Bean
+//    public SecurityWebFilterChain clientSecurityFilterChain(ServerHttpSecurity http) {
+//        return http
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .build();
+//    }
+
+    @Bean
+    WebFilter csrfCookieWebFilter() {
+        return (exchange, chain) -> {
+            Mono<CsrfToken> csrfToken = exchange.getAttributeOrDefault(CsrfToken.class.getName(), Mono.empty());
+            return csrfToken.doOnSuccess(token -> {
+            }).then(chain.filter(exchange));
+        };
+    }
+
+}
