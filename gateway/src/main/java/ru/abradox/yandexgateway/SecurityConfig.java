44412ab2969bf8cf9de652c2f3bfb5bebd41ebc3
karandashov <vlad.karandashov.tech@gmail.com>
2024-01-07 18:30:54 +0300

    вернул cors

--- gateway/src/main/java/ru/abradox/yandexgateway/SecurityConfig.java
+++ gateway/src/main/java/ru/abradox/yandexgateway/SecurityConfig.java
@@ -4,15 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
-import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAttributeHandler;
-import org.springframework.web.server.WebFilter;
-import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -24,26 +18,8 @@ class SecurityConfig {
         return http
                 .oauth2Login(Customizer.withDefaults())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .cors(ServerHttpSecurity.CorsSpec::disable)
                 .authorizeExchange(authorize -> authorize
                         .anyExchange().authenticated())
                 .build();
     }
-
-//    @Bean
-//    public SecurityWebFilterChain clientSecurityFilterChain(ServerHttpSecurity http) {
-//        return http
-//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .build();
-//    }
-
-//    @Bean
-//    WebFilter csrfCookieWebFilter() {
-//        return (exchange, chain) -> {
-//            Mono<CsrfToken> csrfToken = exchange.getAttributeOrDefault(CsrfToken.class.getName(), Mono.empty());
-//            return csrfToken.doOnSuccess(token -> {
-//            }).then(chain.filter(exchange));
-//        };
-//    }
-
 }
